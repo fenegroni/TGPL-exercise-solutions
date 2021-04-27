ff1fa49f0e2b8ff21324d13e934ce5def2562c93
Filippo Negroni <filipponegroni@hotmail.com>
2021-04-27 01:21:42 +0100

    Solve exercise 5.9

---
+++ chapter5/exercise5.9/exercise5.9.go
@@ -0,0 +1,30 @@
+package exercise5_9
+
+import (
+	"strings"
+	"unicode"
+)
+
+func expand(s string, f func(string) string) string {
+	v := new(strings.Builder)
+	for {
+		i := strings.Index(s, "$")
+		if i == -1 {
+			break
+		}
+		v.WriteString(s[:i])
+		s = s[i+1:]
+		t := strings.IndexFunc(s, notIdentifierRune)
+		if t == -1 {
+			t = len(s)
+		}
+		v.WriteString(f(s[:t]))
+		s = s[t:]
+	}
+	v.WriteString(s)
+	return v.String()
+}
+
+func notIdentifierRune(r rune) bool {
+	return !unicode.IsLetter(r)
+}
