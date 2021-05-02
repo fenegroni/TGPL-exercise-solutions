6041b4ecee74b120d8b48ee895b1e70369610827
Filippo Negroni <f.e.negroni@gmail.com>
2021-05-02 06:08:03 +0100

    Implement test for exercise 5.10

--- chapter5/exercise5.10/exercise5.10.go
+++ chapter5/exercise5.10/exercise5.10.go
@@ -1,18 +1,20 @@
 package main
 
-import "fmt"
-import "sort"
+import (
+	"fmt"
+	"sort"
+)
 
 // prereqs maps computer science courses to their prerequisites.
 var prereqs = map[string][]string{
-	"algorithms": {"data structures"},
-	"calculus":   {"linar algebra"},
-	"compilers":  {"data structures", "formal languages", "computer organisation"},
-	"data structures": {"discrete math"},
-	"databases":       {"data structures"},
-	"discrete math":   {"intro to programming"},
-	"formal laguages": {"discrete math"},
-	"networks":        {"operating systems"},
+	"algorithms":            {"data structures"},
+	"calculus":              {"linar algebra"},
+	"compilers":             {"data structures", "formal languages", "computer organisation"},
+	"data structures":       {"discrete math"},
+	"databases":             {"data structures"},
+	"discrete math":         {"intro to programming"},
+	"formal languages":      {"discrete math"},
+	"networks":              {"operating systems"},
 	"operating systems":     {"data structures", "computer organisation"},
 	"programming languages": {"data structures", "computer organisation"},
 }
@@ -24,6 +26,28 @@ func main() {
 }
 
 func topoSort(m map[string][]string) []string {
+	var order []string
+	seen := make(map[string]bool)
+	var visitAll func(items []string)
+	visitAll = func(items []string) {
+		for _, item := range items {
+			if !seen[item] {
+				seen[item] = true
+				visitAll(m[item])
+				order = append(order, item)
+			}
+		}
+	}
+	var keys []string
+	for key := range m {
+		keys = append(keys, key)
+	}
+	sort.Strings(keys)
+	visitAll(keys)
+	return order
+}
+
+func topoSortOrig(m map[string][]string) []string {
 	var order []string
 	seen := make(map[string]bool)
 	var visitAll func(items []string)
--- chapter5/exercise5.10/exercise5.10_test.go
+++ chapter5/exercise5.10/exercise5.10_test.go
@@ -1,7 +1,57 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
-func TestToposort(t *testing.T) {
-	t.Log("Not implemented")
+func TestVerifyToposort(t *testing.T) {
+	tests := []map[string][]string{
+		{"a": {"b"}, "b": {"c"}, "d": {"c"}},
+	}
+	for _, test := range tests {
+		if err := verifyTopologicalSorting(topoSortOrig, test); err != nil {
+			t.Errorf("%v", err)
+		}
+	}
+}
+
+func TestVerifyOriginalToposort(t *testing.T) {
+	graph := map[string][]string{
+		"algorithms":            {"data structures"},
+		"calculus":              {"linar algebra"},
+		"compilers":             {"data structures", "formal languages", "computer organisation"},
+		"data structures":       {"discrete math"},
+		"databases":             {"data structures"},
+		"discrete math":         {"intro to programming"},
+		"formal languages":      {"discrete math"},
+		"networks":              {"operating systems"},
+		"operating systems":     {"data structures", "computer organisation"},
+		"programming languages": {"data structures", "computer organisation"},
+	}
+	if err := verifyTopologicalSorting(topoSort, graph); err != nil {
+		t.Errorf("%v", err)
+	}
+}
+
+func verifyTopologicalSorting(sort func(map[string][]string) []string, graph map[string][]string) error {
+	list := sort(graph)
+	indices := listIndices(list)
+	for k, v := range graph {
+		for _, i := range v {
+			if indices[k] < indices[i] {
+				return fmt.Errorf("%q appears before %q", k, i)
+			}
+		}
+	}
+	return nil
+}
+
+// listIndices maps each value in list to its index.
+func listIndices(list []string) map[string]int {
+	indices := make(map[string]int)
+	for index, value := range list {
+		indices[value] = index
+	}
+	return indices
 }
