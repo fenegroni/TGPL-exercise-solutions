b2bd51cdd2d354152c09433c166d3295f912edd9
Filippo Negroni <filipponegroni@hotmail.com>
2021-05-12 00:10:48 +0100

    Test with two servers

--- chapter5/exercise5.13/exercise5.13_test.go
+++ chapter5/exercise5.13/exercise5.13_test.go
@@ -12,8 +12,22 @@ func TestSavePages(t *testing.T) {
 	// create a handler for the index that returns a basic HTML page
 	// validate the content is saves locally as index.html
 	//
+	server2 := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		switch r.URL.Path {
+		case "/found.html":
+			_, _ = fmt.Fprintln(w, "<html><body><a href=\"too-late.html\">too late</a></body></html>")
+		}
+	}))
+	defer server2.Close()
 	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "<html><body><a href=\"hello.txt\">hello</a></body></html>")
+		switch r.URL.Path {
+		case "/":
+			_, _ = fmt.Fprintln(w, "<html><body><a href=\"hello.html\">hello</a></body></html>")
+		case "/hello.html":
+			_, _ = fmt.Fprintln(w, "<html><body><a href=\"goodbye.html\">goodbye</a></body></html>")
+		case "/goodbye.html":
+			_, _ = fmt.Fprintf(w, "<html><body><a href=\"%s/found.html\">found</a></body></html>", server2.URL)
+		}
 	}))
 	defer server.Close()
 	breadthFirst(crawl, server.Client(), []string{server.URL})
