b4498b626a5a8cd20b6a60678b24b996bec8cb41
Filippo Negroni <f.e.negroni@gmail.com>
2021-07-10 22:34:23 +0100

    implement visit for breadthFirst

--- chapter5/exercise5.14/exercise5.14.go
+++ chapter5/exercise5.14/exercise5.14.go
@@ -2,23 +2,6 @@ package main
 
 import "fmt"
 
-type edges map[string]bool
-type graph map[string]edges
-
-// prereqs maps computer science courses to their prerequisites.
-var prereqs = graph{
-	"algorithms":            {"data structures": true},
-	"calculus":              {"linear algebra": true},
-	"compilers":             {"data structures": true, "formal languages": true, "computer organisation": true},
-	"data structures":       {"discrete math": true},
-	"databases":             {"data structures": true},
-	"discrete math":         {"intro to programming": true},
-	"formal languages":      {"discrete math": true},
-	"networks":              {"operating systems": true},
-	"operating systems":     {"data structures": true, "computer organisation": true},
-	"programming languages": {"data structures": true, "computer organisation": true},
-}
-
 func main() {
 	fmt.Println("exercise5.14")
 }
@@ -26,4 +9,16 @@ func main() {
 // breadthFirst calls f for each item in the worklist.
 // Any items returned by f are added to the worklist.
 // f is called at most once for each item.
-func breadthFirst(f func(string) []string,
+func breadthFirst(f func(string) []string, worklist []string) {
+	seen := make(map[string]bool)
+	for len(worklist) > 0 {
+		items := worklist
+		worklist = nil
+		for _, item := range items {
+			if !seen[item] {
+				seen[item] = true
+				worklist = append(worklist, f(item)...)
+			}
+		}
+	}
+}
--- chapter5/exercise5.14/exercise5.14_test.go
+++ chapter5/exercise5.14/exercise5.14_test.go
@@ -2,6 +2,29 @@ package main
 
 import "testing"
 
-func TestBreadthFirstPrereqs(t *testing.T) {
+// prereqs maps computer science courses to their prerequisites.
+var prereqs = map[string][]string{
+	"algorithms":            {"data structures"},
+	"calculus":              {"linear algebra"},
+	"compilers":             {"data structures", "formal languages", "computer organisation"},
+	"data structures":       {"discrete math"},
+	"databases":             {"data structures"},
+	"discrete math":         {"intro to programming"},
+	"formal languages":      {"discrete math"},
+	"networks":              {"operating systems"},
+	"operating systems":     {"data structures", "computer organisation"},
+	"programming languages": {"data structures", "computer organisation"},
+}
 
+func
+
+func TestBreadthFirstPrereqs(t *testing.T) {
+	visit := func(s string) []string {
+
+	}
+	var courses []string
+	for k := range prereqs {
+		courses = append(courses, k)
+	}
+	breadthFirst(visit, courses)
 }
