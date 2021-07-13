23ed914f3da1cf6ff8e8d7dcee4640fc0a7dd1f0
Filippo Negroni <filipponegroni@hotmail.com>
2021-07-13 08:26:04 +0100

    Begin exercise 5.14
    Explain what `breadthFirst` does and set the scene for the test
    using `prereqs` from exercise 5.11.

--- chapter5/exercise5.14/exercise5.14_test.go
+++ chapter5/exercise5.14/exercise5.14_test.go
@@ -3,22 +3,20 @@ package main
 import "testing"
 
 // prereqs maps computer science courses to their prerequisites.
-var prereqs = map[string][]string{
-	"algorithms":            {"data structures"},
-	"calculus":              {"linear algebra"},
-	"compilers":             {"data structures", "formal languages", "computer organisation"},
-	"data structures":       {"discrete math"},
-	"databases":             {"data structures"},
-	"discrete math":         {"intro to programming"},
-	"formal languages":      {"discrete math"},
-	"networks":              {"operating systems"},
-	"operating systems":     {"data structures", "computer organisation"},
-	"programming languages": {"data structures", "computer organisation"},
-}
+func ExampleBreadthFirst() {
+	var prereqs = map[string][]string{
+		"algorithms":            {"data structures"},
+		"calculus":              {"linear algebra"},
+		"compilers":             {"data structures", "formal languages", "computer organisation"},
+		"data structures":       {"discrete math"},
+		"databases":             {"data structures"},
+		"discrete math":         {"intro to programming"},
+		"formal languages":      {"discrete math"},
+		"networks":              {"operating systems"},
+		"operating systems":     {"data structures", "computer organisation"},
+		"programming languages": {"data structures", "computer organisation"},
+	}
 
-func
-
-func TestBreadthFirstPrereqs(t *testing.T) {
 	visit := func(s string) []string {
 
 	}
