26959d633dd27585108fb875d9d22c4d37f4dc57
Filippo Negroni <filipponegroni@hotmail.com>
2021-07-14 08:06:17 +0100

    Pass first test case

--- chapter5/exercise5.15/exercise5.15.go
+++ chapter5/exercise5.15/exercise5.15.go
@@ -1,1 +1,15 @@
 package exercise5_15
+
+func Min(vals ...int) int {
+	min := vals[0]
+	for _, v := range vals[1:] {
+		if v < min {
+			min = v
+		}
+	}
+	return min
+}
+
+func Max(vals ...int) int {
+	return 0
+}
--- chapter5/exercise5.15/exercise5.15_test.go
+++ chapter5/exercise5.15/exercise5.15_test.go
@@ -1,1 +1,21 @@
 package exercise5_15
+
+import "testing"
+
+func TestMin(t *testing.T) {
+	tests := []struct {
+		vals []int
+		want int
+	}{
+		{[]int{1, 2}, 1},
+	}
+	for _, test := range tests {
+		if got := Min(test.vals...); got != test.want {
+			t.Errorf("Min(%v) = %d, want %d", test.vals, got, test.want)
+		}
+	}
+}
+
+func TestMax(t *testing.T) {
+
+}
