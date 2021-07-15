e996ef9d6c71f98dab4fe6fa3fe195e5b6462038
Filippo Negroni <filipponegroni@hotmail.com>
2021-07-15 05:04:47 +0100

    Implement Min and Max properly

--- chapter5/exercise5.15/exercise5.15.go
+++ chapter5/exercise5.15/exercise5.15.go
@@ -1,8 +1,8 @@
 package exercise5_15
 
-func FastMin(vals ...int) int {
-	min := vals[0]
-	for _, v := range vals[1:] {
+func Min(v1 int, vn ...int) int {
+	min := v1
+	for _, v := range vn {
 		if v < min {
 			min = v
 		}
@@ -10,19 +10,12 @@ func FastMin(vals ...int) int {
 	return min
 }
 
-func Min(vals ...int) (int, bool) {
-	if len(vals) == 0 {
-		return 0, false
-	}
-	min := vals[0]
-	for _, v := range vals[1:] {
-		if v < min {
-			min = v
+func Max(v1 int, vn ...int) int {
+	max := v1
+	for _, v := range vn {
+		if v > max {
+			max = v
 		}
 	}
-	return min, true
-}
-
-func Max(vals ...int) int {
-	return 0
+	return max
 }
--- chapter5/exercise5.15/exercise5.15_test.go
+++ chapter5/exercise5.15/exercise5.15_test.go
@@ -2,53 +2,48 @@ package exercise5_15
 
 import "testing"
 
-func TestFastMin(t *testing.T) {
-	tests := []struct {
-		vals []int
-		want int
-	}{
-		{[]int{1}, 1},
-		{[]int{1, 2, 3}, 1},
-		{[]int{3, 1, 2}, 1},
-		{[]int{0}, 0},
-		{[]int{-1, -2}, -2},
-		{[]int{-1, -3, 2, -4, 5}, -4},
-	}
-	for _, test := range tests {
-		if got := FastMin(test.vals...); got != test.want {
-			t.Errorf("Min(%v) = %d, want %d", test.vals, got, test.want)
-		}
-	}
-}
-
 func TestMin(t *testing.T) {
+	if 1 != Min(1) {
+		t.Errorf("Min(1) != 1")
+	}
 	tests := []struct {
-		vals []int
+		v1   int
+		vn   []int
 		want int
 	}{
-		{[]int{1}, 1},
-		{[]int{1, 2, 3}, 1},
-		{[]int{3, 1, 2}, 1},
-		{[]int{0}, 0},
-		{[]int{-1, -2}, -2},
-		{[]int{-1, -3, 2, -4, 5}, -4},
+		{1, nil, 1},
+		{1, []int{}, 1},
+		{1, []int{2, 3}, 1},
+		{3, []int{1, 2}, 1},
+		{-1, []int{-2}, -2},
+		{-1, []int{-3, 2, 0, -4, 5}, -4},
 	}
 	for _, test := range tests {
-		if got, ok := Min(test.vals...); ok && got != test.want {
-			t.Errorf("Min(%v) = {%t; %d}, want {true, %d}", test.vals, ok, got, test.want)
+		if got := Min(test.v1, test.vn...); got != test.want {
+			t.Errorf("Min(%v, %v) = %d, want %d", test.v1, test.vn, got, test.want)
 		}
-	}
-	if v, ok := Min(); ok {
-		t.Errorf("Min() = {%d, true}, want {%d, false}", v, v)
-	}
-	if v, ok := Min(nil...); ok {
-		t.Errorf("Min(nil...) = {%d, true}, want {%d, false}", v, v)
-	}
-	if v, ok := Min([]int{}...); ok {
-		t.Errorf("Min([]int{}...]) = {%d, true}, want {%d, false}", v, v)
 	}
 }
 
 func TestMax(t *testing.T) {
-
+	if 1 != Max(1) {
+		t.Errorf("Max(1) != 1")
+	}
+	tests := []struct {
+		v1   int
+		vn   []int
+		want int
+	}{
+		{1, nil, 1},
+		{1, []int{}, 1},
+		{1, []int{2, 3}, 3},
+		{3, []int{1, 2}, 3},
+		{-1, []int{-2}, -1},
+		{-1, []int{-3, 2, 0, -4, 5}, 5},
+	}
+	for _, test := range tests {
+		if got := Max(test.v1, test.vn...); got != test.want {
+			t.Errorf("Max(%v, %v) = %d, want %d", test.v1, test.vn, got, test.want)
+		}
+	}
 }
