280d777205d5db28146959e305e631ecca1a299a
Filippo Erik Negroni <filipponegroni@hotmail.com>
2021-07-15 05:25:18 +0100

    Exercise5.15 (#8)
    Implement exercise 5.15

---
+++ chapter5/exercise5.15/exercise5.15.go
@@ -0,0 +1,21 @@
+package exercise5_15
+
+func Min(v1 int, vn ...int) int {
+	min := v1
+	for _, v := range vn {
+		if v < min {
+			min = v
+		}
+	}
+	return min
+}
+
+func Max(v1 int, vn ...int) int {
+	max := v1
+	for _, v := range vn {
+		if v > max {
+			max = v
+		}
+	}
+	return max
+}
