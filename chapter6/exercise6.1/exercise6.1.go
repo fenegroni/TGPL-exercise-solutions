2e75bb94889f7348646d64254c526f84dd863a31
Filippo Negroni <f.e.negroni@gmail.com>
2021-07-29 20:05:56 +0100

    Begin exercise 6.1

---
+++ chapter6/exercise6.1/exercise6.1.go
@@ -0,0 +1,34 @@
+// Exercise5.1 prints the links in an HTML document read from standard input.
+package main
+
+import (
+	"fmt"
+	"golang.org/x/net/html"
+	"os"
+)
+
+func main() {
+	doc, err := html.Parse(os.Stdin)
+	if err != nil {
+		_, _ = fmt.Fprintf(os.Stderr, "exercise5.1: %v\n", err)
+		os.Exit(1)
+	}
+	for _, link := range Visit(nil, doc) {
+		fmt.Println(link)
+	}
+}
+
+// Visit appends to links each link found in n and returns the result.
+func Visit(links []string, n *html.Node) []string {
+	if n == nil {
+		return links
+	}
+	if n.Type == html.ElementNode && n.Data == "a" {
+		for _, a := range n.Attr {
+			if a.Key == "href" {
+				links = append(links, a.Val)
+			}
+		}
+	}
+	return Visit(Visit(links, n.FirstChild), n.NextSibling)
+}
