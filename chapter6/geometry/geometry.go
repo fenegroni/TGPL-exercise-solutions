28cc54036af3b80c52e2c2310f02595f368cc860
Filippo Negroni <f.e.negroni@gmail.com>
2021-07-30 07:49:14 +0100

    Add type Path

--- chapter6/geometry/geometry.go
+++ chapter6/geometry/geometry.go
@@ -13,3 +13,16 @@ func Distance(p, q Point) float64 {
 func (p Point) Distance(q Point) float64 {
 	return math.Hypot(q.X-p.X, q.Y-p.Y)
 }
+
+type Path []Point
+
+// The Distance travelled along the path.
+func (p Path) Distance() float64 {
+	sum := 0.0
+	for i := range p {
+		if i > 0 {
+			sum += p[i-1].Distance(p[i])
+		}
+	}
+	return sum
+}
--- chapter6/main.go
+++ chapter6/main.go
@@ -6,7 +6,13 @@ import (
 )
 
 func main() {
-	var p, q geometry.Point
-	fmt.Printf("The distance between %v and %v is %v", p, q, p.Distance(q))
-	fmt.Printf("The distance between %v and %v is %v", p, q, geometry.Distance(p, q))
+	p := geometry.Point{X: 0, Y: 0}
+	q := geometry.Point{X: 1, Y: 0}
+	fmt.Printf("The distance between %v and %v is %v\n", p, q, p.Distance(q))
+	fmt.Printf("The distance between %v and %v is %v\n", q, p, geometry.Distance(q, p))
+
+	var path geometry.Path
+	r := geometry.Point{X: 0, Y: 1}
+	path = append(path, p, q, r)
+	fmt.Printf("The distance on path %v is %v\n", path, path.Distance())
 }
