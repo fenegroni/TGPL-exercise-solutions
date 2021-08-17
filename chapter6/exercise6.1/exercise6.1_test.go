1b1c26b0fbf7c4722d31d81bb93ea3aabae8a56c
Filippo Negroni <filipponegroni@hotmail.com>
2021-08-17 22:41:04 +0100

    Test IntSet using a test table

--- chapter6/exercise6.1/exercise6.1_test.go
+++ chapter6/exercise6.1/exercise6.1_test.go
@@ -3,8 +3,52 @@ package exercise6_1
 import "testing"
 
 func TestIntSet(t *testing.T) {
-	// TODO tests table using String representation
-	// use function expressions etc to point to methods and arguments
+	type operation int
+	const (
+		add operation = iota
+		remove
+		clear
+	)
+	type action struct {
+		op  operation
+		val []int
+	}
+	tests := []struct {
+		actions []action
+		want    string
+	}{
+		{[]action{{add, []int{1, 2}}}, "{1 2}"},
+		{[]action{{add, []int{0, 1000}}}, "{0 1000}"},
+		{[]action{
+			{add, []int{1, 1000}},
+			{remove, []int{1, 1000}},
+		}, "{}"},
+		{[]action{
+			{add, []int{1, 1000}},
+			{remove, []int{1000}},
+			{add, []int{100}},
+		}, "{1 100}"},
+	}
+	for _, test := range tests {
+		s := new(IntSet)
+		for _, a := range test.actions {
+			switch a.op {
+			case add:
+				for _, v := range a.val {
+					s.Add(v)
+				}
+			case remove:
+				for _, v := range a.val {
+					s.Remove(v)
+				}
+			case clear:
+				s.Clear()
+			}
+		}
+		if got := s.String(); got != test.want {
+			t.Errorf("%v = %s, want %s", test.actions, got, test.want)
+		}
+	}
 }
 
 func TestIntSet_Len_emptyset(t *testing.T) {
