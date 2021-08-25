73a909f678f3baba9cdf289820adbdb777353a4e
Filippo Negroni <filipponegroni@hotmail.com>
2021-08-25 22:30:04 +0100

    Complete ex 6.2

--- chapter6/exercise6.2/exercise6.2_test.go
+++ chapter6/exercise6.2/exercise6.2_test.go
@@ -43,5 +43,55 @@ func TestIntSet(t *testing.T) {
 }
 
 func TestIntSet_AddAll(t *testing.T) {
+	v := new(IntSet)
+	v.AddAll()
+	want := "{}"
+	if got := v.String(); got != want {
+		t.Fatalf("{}.AddAll() = %s, want %s", got, want)
+	}
+	v.AddAll(1)
+	want = "{1}"
+	if got := v.String(); got != want {
+		t.Fatalf("{}.AddAll() = %s, want %s", got, want)
+	}
+	v.AddAll(1, 100, 1000)
+	want = "{1 100 1000}"
+	if got := v.String(); got != want {
+		t.Fatalf("{1}.AddAll(1, 100, 1000) = %s, want %s", got, want)
+	}
+}
 
+func TestIntSet_RemoveAll(t *testing.T) {
+	v := new(IntSet)
+	v.RemoveAll(1)
+	want := "{}"
+	if got := v.String(); got != want {
+		t.Fatalf("{}.RemoveAll(1) = %s, want %s", got, want)
+	}
+	v.Add(1)
+	v.RemoveAll()
+	want = "{1}"
+	if got := v.String(); got != want {
+		t.Fatalf("{1}.RemoveAll() = %s, want %s", got, want)
+	}
+	v.RemoveAll(1)
+	want = "{}"
+	if got := v.String(); got != want {
+		t.Fatalf("{1}.RemoveAll(1) = %s, want %s", got, want)
+	}
+	v.Add(100)
+	v.Add(200)
+	v.Add(300)
+	v.RemoveAll(300, 200)
+	want = "{100}"
+	if got := v.String(); got != want {
+		t.Fatalf("{100 200 300}.Remove(300, 200) = %s, want %s", got, want)
+	}
+	v.Add(1000)
+	v.Add(2000)
+	v.RemoveAll(100, 1000)
+	want = "{2000}"
+	if got := v.String(); got != want {
+		t.Fatalf("{100 1000 2000}.Remove(100, 1000) = %s, want %s", got, want)
+	}
 }
