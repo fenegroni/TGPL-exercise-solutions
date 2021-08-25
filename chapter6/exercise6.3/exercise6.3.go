0aac1a2ad5fb51ad30c9b077a31664be2bee7685
Filippo Negroni <filipponegroni@hotmail.com>
2021-08-25 23:39:55 +0100

    Begin exercise 6.3

---
+++ chapter6/exercise6.3/exercise6.3.go
@@ -0,0 +1,97 @@
+package exercise6_3
+
+import (
+	"fmt"
+	"strings"
+)
+
+// An IntSet is a set of small non-negative integers.
+// Its zero value represents the empty set.
+type IntSet struct {
+	words []uint64
+}
+
+// Add the non-negative value x to the set
+func (s *IntSet) Add(x int) {
+	word, bit := x/64, uint(x%64)
+	for word >= len(s.words) {
+		s.words = append(s.words, 0)
+	}
+	s.words[word] |= 1 << bit
+}
+
+// Remove the non-negative value x from the set.
+func (s *IntSet) Remove(x int) {
+	word, bit := x/64, uint(x%64)
+	if word < len(s.words) {
+		s.words[word] &^= 1 << bit
+	}
+}
+
+// Clear removes all elements from the set
+func (s *IntSet) Clear() {
+	for i := range s.words {
+		s.words[i] = 0
+	}
+}
+
+// AddAll adds the non-negative values xs to the set
+func (s *IntSet) AddAll(xs ...int) {
+	for _, x := range xs {
+		s.Add(x)
+	}
+}
+
+// RemoveAll removes all the non-negative values xs from the set
+func (s *IntSet) RemoveAll(xs ...int) {
+	for _, x := range xs {
+		s.Remove(x)
+	}
+}
+
+// UnionWith sets s to the union of s and t
+func (s *IntSet) UnionWith(t *IntSet) {
+	for i, tword := range t.words {
+		if i < len(s.words) {
+			s.words[i] |= tword
+		} else {
+			s.words = append(s.words, tword)
+		}
+	}
+}
+
+// IntersectWith sets s to the intersection of s and t
+func (s *IntSet) IntersectWith(t *IntSet) {
+	s.UnionWith(t)
+}
+
+// DifferenceWith sets s to the difference of s and t
+func (s *IntSet) DifferenceWith(t *IntSet) {
+	s.UnionWith(t)
+}
+
+// SymmetricDifference sets s to the symmetric difference of s and t
+func (s *IntSet) SymmetricDifference(t *IntSet) {
+	s.UnionWith(t)
+}
+
+// String returns the set as a string of the form "{1 2 3}"
+func (s *IntSet) String() string {
+	var buf strings.Builder
+	buf.WriteString("{")
+	for i, word := range s.words {
+		if word == 0 {
+			continue
+		}
+		for j := 0; j < 64; j++ {
+			if word&(1<<uint(j)) != 0 {
+				if buf.Len() > len("{") {
+					buf.WriteString(" ")
+				}
+				buf.WriteString(fmt.Sprintf("%d", 64*i+j))
+			}
+		}
+	}
+	buf.WriteString("}")
+	return buf.String()
+}
---
+++ chapter6/exercise6.3/exercise6.3_test.go
@@ -0,0 +1,19 @@
+package exercise6_3
+
+import "testing"
+
+func TestIntSet_UnionWith(t *testing.T) {
+
+}
+
+func TestIntSet_IntersectWith(t *testing.T) {
+
+}
+
+func TestIntSet_DifferenceWith(t *testing.T) {
+
+}
+
+func TestIntSet_SymmetricDifference(t *testing.T) {
+
+}
