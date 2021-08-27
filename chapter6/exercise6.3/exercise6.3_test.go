43df0aa4ff22e5a6bb6bc5e8ce95a417b4300c90
Filippo Negroni <filipponegroni@hotmail.com>
2021-08-27 08:35:40 +0100

    ex6.3 Fail IntersectWith
    Refactored TestIntSet_IntersectWith
    {0 1000}.IntersectWith({0}) = {0 1000}, want {0}

--- chapter6/exercise6.3/exercise6.3_test.go
+++ chapter6/exercise6.3/exercise6.3_test.go
@@ -16,17 +16,26 @@ func TestIntSet_UnionWith(t *testing.T) {
 }
 
 func TestIntSet_IntersectWith(t *testing.T) {
-	v, p := new(IntSet), new(IntSet)
-	v.AddAll(1, 2, 3, 4)
-	p.AddAll(4, 5, 6, 7)
-	vStr := v.String()
-	pStr := p.String()
-	v.IntersectWith(p)
-	want := "{4}"
-	if got := v.String(); got != want {
-		t.Fatalf("%s.IntersectWith(%s) = %s, want %s", vStr, pStr, got, want)
+	tests := []struct {
+		v, p []int
+		want string
+	}{
+		{[]int{1, 2}, []int{2, 3}, "{2}"},
+		{[]int{1, 2}, []int{3, 4}, "{}"},
+		{[]int{0, 1000}, []int{1000}, "{1000}"},
+		{[]int{0, 1000}, []int{0}, "{0}"},
 	}
-
+	for _, test := range tests {
+		v, p := new(IntSet), new(IntSet)
+		v.AddAll(test.v...)
+		p.AddAll(test.p...)
+		vStr := v.String()
+		pStr := p.String()
+		v.IntersectWith(p)
+		if got := v.String(); got != test.want {
+			t.Fatalf("%s.IntersectWith(%s) = %s, want %s", vStr, pStr, got, test.want)
+		}
+	}
 }
 
 func TestIntSet_DifferenceWith(t *testing.T) {
