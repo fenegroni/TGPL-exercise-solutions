c3ee798cc73dab3bb0ecdea93ca2385aec6ceedf
Filippo Negroni <filipponegroni@hotmail.com>
2021-09-04 00:31:45 +0100

    ex6.4 Pass and complete TestIntSet_Elems
    Also add ExampleIntSet_Elems

--- chapter6/exercise6.4/exercise6.4_test.go
+++ chapter6/exercise6.4/exercise6.4_test.go
@@ -1,13 +1,14 @@
 package exercise6_4
 
 import (
+	"fmt"
 	"reflect"
 	"testing"
 )
 
 func TestIntSet_Elems(t *testing.T) {
 	tests := [][]int{
-		[]int{},
+		[]int(nil),
 		[]int{1000},
 		[]int{1, 2},
 		[]int{0, 1000},
@@ -18,7 +19,19 @@ func TestIntSet_Elems(t *testing.T) {
 		v := new(IntSet)
 		v.AddAll(test...)
 		if got := v.Elems(); !reflect.DeepEqual(got, test) {
-			t.Fatalf("%s.Elems() = %v, want %v", v, got, test)
+			t.Errorf("%s.Elems() = %v, want %v", v, got, test)
 		}
 	}
+}
+
+func ExampleIntSet_Elems() {
+	v := new(IntSet)
+	v.AddAll(200, 3000, 1)
+	for _, e := range v.Elems() {
+		fmt.Println(e)
+	}
+	// Output:
+	// 1
+	// 200
+	// 3000
 }
