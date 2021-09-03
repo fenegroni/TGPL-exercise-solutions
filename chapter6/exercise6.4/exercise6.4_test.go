7e48761382623ba529624c56f2f9f9cd40a355e2
Filippo Negroni <filipponegroni@hotmail.com>
2021-09-03 23:43:52 +0100

    ex6.4 Fail TestIntSet_Elems
    {}.Elems() = [], want []
    Looks like the empty set Elems returns something else.

--- chapter6/exercise6.4/exercise6.4_test.go
+++ chapter6/exercise6.4/exercise6.4_test.go
@@ -6,10 +6,19 @@ import (
 )
 
 func TestIntSet_Elems(t *testing.T) {
-	v := new(IntSet)
-	elems := []int{1, 2, 3, 4}
-	v.AddAll(elems...)
-	if got := v.Elems(); !reflect.DeepEqual(got, elems) {
-		t.Fatalf("%s.Elems() = %v, want %v", v, got, elems)
+	tests := [][]int{
+		[]int{},
+		[]int{1000},
+		[]int{1, 2},
+		[]int{0, 1000},
+		[]int{1000},
+		[]int{1, 200, 3000, 40000},
+	}
+	for _, test := range tests {
+		v := new(IntSet)
+		v.AddAll(test...)
+		if got := v.Elems(); !reflect.DeepEqual(got, test) {
+			t.Fatalf("%s.Elems() = %v, want %v", v, got, test)
+		}
 	}
 }
