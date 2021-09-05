26f86a48244273f1328db38235a93013711b0bd6
Filippo Negroni <filipponegroni@hotmail.com>
2021-09-05 16:05:57 +0100

    ex7.1 Fail TestWordCounter
    Implement Write the same as for ByteCounter:
    w is 12, want 2

--- chapter7/exercise7.1/exercise7.1.go
+++ chapter7/exercise7.1/exercise7.1.go
@@ -9,4 +9,9 @@ func (c *ByteCounter) Write(p []byte) (int, error) {
 
 type WordCounter int
 
+func (w *WordCounter) Write(p []byte) (int, error) {
+	*w += WordCounter(len(p))
+	return len(p), nil
+}
+
 type LineCounter int
