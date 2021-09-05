51590f1de11d2e6deeb4cb42eb7135df4cb7245f
Filippo Negroni <filipponegroni@hotmail.com>
2021-09-05 15:43:26 +0100

    ex7.1 Pass TestByteCounter_Write

--- chapter7/exercise7.1/exercise7.1_test.go
+++ chapter7/exercise7.1/exercise7.1_test.go
@@ -20,4 +20,11 @@ func TestByteCounter_Write(t *testing.T) {
 	if got := fmt.Sprint(c); got != want {
 		t.Errorf("c is %s, want %s", got, want)
 	}
+	c = 0 // reset the counter
+	var name = "Dolly"
+	fmt.Fprintf(&c, "hello, %s", name)
+	want = "12"
+	if got := fmt.Sprint(c); got != want {
+		t.Errorf("c is %s, want %s", got, want)
+	}
 }
