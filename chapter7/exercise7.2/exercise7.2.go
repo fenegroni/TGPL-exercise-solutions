63197211897f3334626b3c15c7c1ec26ac352684
Filippo Negroni <filipponegroni@hotmail.com>
2021-09-06 02:19:13 +0100

    ex7.2 Implement CountingWriter

---
+++ chapter7/exercise7.2/exercise7.2.go
@@ -0,0 +1,7 @@
+package exercise7_2
+
+import "io"
+
+func CountingWriter(w io.Writer) (io.Writer, *int64) {
+	return nil, nil
+}
--- chapter7/exercise7.2/exercise7.2_test.go
+++ chapter7/exercise7.2/exercise7.2_test.go
@@ -1,9 +1,11 @@
 package exercise7_2
 
 import (
+	"os"
 	"testing"
 )
 
 func TestCountingWriter(t *testing.T) {
-
+	w, c := CountingWriter(os.Stdout)
+	t.Logf("%v, %v", w, c)
 }
