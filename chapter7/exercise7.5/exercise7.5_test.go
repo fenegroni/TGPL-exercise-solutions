fc3e8e52931f6405b18a13b6326373e3022c906e
Filippo Negroni <filipponegroni@hotmail.com>
2021-09-09 23:02:34 +0100

    ex7.5 Implement TestLimitedReader
    The test exercises the original implementation in the `io` package and discovers something interesting.

--- chapter7/exercise7.5/exercise7.5_test.go
+++ chapter7/exercise7.5/exercise7.5_test.go
@@ -1,9 +1,39 @@
 package exercise7_5
 
 import (
+	"io"
 	"testing"
 )
 
-func TestLimitReader(t *testing.T) {
+type zero struct{}
 
+func (*zero) Read(p []byte) (n int, err error) {
+	for i := 0; i < len(p); i++ {
+		p[i] = 0
+	}
+	return len(p), nil
+}
+
+func TestLimitReader(t *testing.T) {
+	tests := []struct {
+		limit int64
+	}{
+		{0},
+		{1},
+		{2},
+		{1000},
+	}
+	var z zero
+	for _, test := range tests {
+		eofZero := io.LimitReader(&z, test.limit)
+		p := make([]byte, test.limit+10)
+		n, err := eofZero.Read(p)
+		n2, err2 := eofZero.Read(p)
+		if int64(n) > test.limit || (err != io.EOF && err != nil) {
+			t.Errorf("first pass io.LimitReader(%d).Read(%d) returns (%d, %v) want (%d, %v)", test.limit, len(p), n, err, test.limit, io.EOF)
+		}
+		if int64(n2) > 0 || err2 != io.EOF {
+			t.Errorf("second pass io.LimitReader(%d).Read(%d) returns (%d, %v) want (%d, %v)", test.limit, len(p), n, err, 0, io.EOF)
+		}
+	}
 }
