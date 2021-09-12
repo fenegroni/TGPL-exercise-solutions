4d58db5cc31f5c9b3cc6f1182e8bdd386c58aa56
Filippo Negroni <f.e.negroni@gmail.com>
2021-09-12 10:11:38 +0100

    ex7.5 Pass TestLimitReader
    LimitedReader passes the test.
    But the test is brittle.

--- chapter7/exercise7.5/exercise7.5.go
+++ chapter7/exercise7.5/exercise7.5.go
@@ -2,6 +2,25 @@ package exercise7_5
 
 import "io"
 
+type LimitedReader struct {
+	r        io.Reader
+	limit, n int64
+}
+
+func (lr *LimitedReader) Read(p []byte) (int, error) {
+	if lr.n == lr.limit {
+		return 0, io.EOF
+	}
+	q := p[:lr.limit-lr.n]
+	n, err := lr.r.Read(q)
+	lr.n += int64(n)
+	if err == nil && lr.n == lr.limit {
+		err = io.EOF
+	}
+	return n, err
+}
+
 func LimitReader(r io.Reader, limit int64) io.Reader {
-	return r
+	var lr = LimitedReader{r, limit, 0}
+	return &lr
 }
