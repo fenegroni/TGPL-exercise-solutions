d7dbf7fd43a3be6690d078066fb4228335694514
Filippo Negroni <filipponegroni@hotmail.com>
2021-09-16 08:19:29 +0100

    ex7.6 test tempconv outside tempconv using flag

--- chapter7/exercise7.6/exercise7.6_test.go
+++ chapter7/exercise7.6/exercise7.6_test.go
@@ -1,7 +1,13 @@
 package exercise7_6
 
-import "testing"
+import (
+	tempconv "TGPL-exercise-solutions/chapter7/exercise7.6/temconv"
+	"flag"
+	"fmt"
+	"testing"
+)
 
-func TestPrint(t *testing.T) {
-	print()
+func TestSetCelsius(t *testing.T) {
+	flag.Parse()
+	fmt.Println(tempconv.AbsoluteZeroC)
 }
