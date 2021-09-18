1ae50938732602ff4e5cca33c0fdb5e61dce588b
Filippo Negroni <f.e.negroni@gmail.com>
2021-09-18 18:07:29 +0100

    ex7.8 Empty test

--- chapter7/exercise7.8/exercise7.8_test.go
+++ chapter7/exercise7.8/exercise7.8_test.go
@@ -1,21 +1,8 @@
 package exercise7_7
 
 import (
-	"TGPL-exercise-solutions/chapter7/exercise7.6/tempconv"
-	"flag"
-	"strings"
 	"testing"
 )
 
 func TestCelsiusFlag_prettyPrint(t *testing.T) {
-	flags := flag.NewFlagSet("test", flag.ContinueOnError)
-	var out strings.Builder
-	flags.SetOutput(&out)
-	_ = tempconv.CelsiusFlag(flags, "temp", 20, "the temperature")
-	flags.PrintDefaults()
-	want := "(default 20°C)"
-	got := out.String()
-	if !strings.Contains(got, want) {
-		t.Errorf("Defaults output does not contain %q, got %q", want, got)
-	}
 }
