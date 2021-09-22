470294840bfb42222aa1100660090a6a339715c8
Filippo Negroni <filipponegroni@hotmail.com>
2021-09-22 08:16:05 +0100

    ex7.8 trying to define the test and its types

--- chapter7/exercise7.8/exercise7.8_test.go
+++ chapter7/exercise7.8/exercise7.8_test.go
@@ -1,71 +1,22 @@
-package exercise7_7
+package exercise7_8
 
 import (
-	"TGPL-exercise-solutions/chapter7/exercise7.8/music"
-	"fmt"
-	"sort"
 	"testing"
-	"time"
 )
 
-var tracks = []*music.Track{
-	{"Go", "Delilah", "From the Roots Up", 2012, length("3m38s")},
-	{"Go", "Moby", "Moby", 1992, length("3m37s")},
-	{"Go Ahead", "Alicia Keys", "As I Am", 2007, length("4m36s")},
-	{"Ready 2 Go", "Martin Solveig", "Smash", 2011, length("4m24s")},
+type ThreeColumns struct {
+	A, B, C int
 }
 
-func length(s string) time.Duration {
-	d, err := time.ParseDuration(s)
-	if err != nil {
-		panic(s)
-	}
-	return d
+type StableTable struct {
+	data []*ThreeColumns
 }
 
-func TestPrintTracks(t *testing.T) {
-	fmt.Println("Click Artist once:")
-	sort.Sort(music.ByArtist(tracks))
-	music.PrintTracks(tracks)
-	fmt.Println("Click Artist twice:")
-	sort.Sort(sort.Reverse(music.ByArtist(tracks)))
-	music.PrintTracks(tracks)
-	fmt.Println("Click Year once:")
-	sort.Sort(music.ByYear(tracks))
-	music.PrintTracks(tracks)
-	fmt.Println("Click Year twice:")
-	sort.Sort(sort.Reverse(music.ByYear(tracks)))
-	music.PrintTracks(tracks)
-	fmt.Println("Custom sorting: Title, Year, Length:")
-	sort.Sort(music.CustomSort{T: tracks, CombiLess: func(x, y *music.Track) bool {
-		if x.Title != y.Title {
-			return x.Title < y.Title
-		}
-		if x.Year != y.Year {
-			return x.Year < y.Year
-		}
-		if x.Length != y.Length {
-			return x.Length < y.Length
-		}
-		return false
-	}})
-	music.PrintTracks(tracks)
-}
-
-func TestIntSort(t *testing.T) {
-	values := []int{3, 1, 4, 1}
-	if false != sort.IntsAreSorted(values) {
-		t.Errorf("values are sorted: %v", values)
+func TestStableSortTracks(t *testing.T) {
+	var table = StableTable{
+		[]*ThreeColumns{{1, 2, 3}},
 	}
-	sort.Ints(values)
-	if true != sort.IntsAreSorted(values) {
-		t.Errorf("values are not sorted: %v", values)
-	}
-	sort.Sort(sort.Reverse(sort.IntSlice(values)))
-	if true != sort.IntsAreSorted(values) {
-		t.Errorf("values are not sorted by 'int' ascending sorted: %v", values)
-	}
-	if true != sort.IsSorted(sort.IntSlice(values)) {
-		t.Errorf("values are not sorted: %v", values)
-	}
+	// want will show what we expect the order to be
+	// ordering by column A then column B is it different if the ordering is not kept?
+	// as we put tests together we can validate it.
 }
