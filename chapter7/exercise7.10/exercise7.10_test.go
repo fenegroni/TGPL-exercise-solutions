0a9bb253336f7f1051c22281bf4ccb88892f70ba
Filippo Negroni <filipponegroni@hotmail.com>
2021-12-26 13:33:35 +0000

    ex7.10 Test IsPalindrome with []rune and string
    Additionally, test a case insensitive []rune type to provide an example.

--- chapter7/exercise7.10/exercise7.10_test.go
+++ chapter7/exercise7.10/exercise7.10_test.go
@@ -1,6 +1,10 @@
 package exercise7_10
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+	"unicode"
+)
 
 type RuneSlice []rune
 
@@ -16,7 +20,7 @@ func (r RuneSlice) Swap(i, j int) {
 	r[i], r[j] = r[j], r[i]
 }
 
-func TestIsPalindrome(t *testing.T) {
+func TestIsPalindromeWithRuneSlice(t *testing.T) {
 	tests := []struct {
 		s    RuneSlice
 		want bool
@@ -36,6 +40,72 @@ func TestIsPalindrome(t *testing.T) {
 			t.Errorf("IsPalindrome(%q): got %t, want %t", test.s, got, test.want)
 		}
 	}
-	// TODO use type string
-	// TODO try case insensitive modifiers
+}
+
+type String string
+
+func (s String) Len() int {
+	return len(s)
+}
+
+func (s String) Less(i, j int) bool {
+	return s[i] < s[j]
+}
+
+func (s String) Swap(int, int) {
+	// strings are immutable
+	panic(fmt.Sprintf("Illegal call to String.Swap"))
+}
+
+func TestIsPalindromeWithString(t *testing.T) {
+	tests := []struct {
+		s    String
+		want bool
+	}{
+		{String(""), true},
+		{String("a"), true},
+		{String("ab"), false},
+		{String("aba"), true},
+		{String("abc"), false},
+		{String("abba"), true},
+		{String("abcba"), true},
+		{String("abcda"), false},
+	}
+	for _, test := range tests {
+		got := IsPalindrome(test.s)
+		if got != test.want {
+			t.Errorf("IsPalindrome(%q): got %t, want %t", test.s, got, test.want)
+		}
+	}
+}
+
+type CaseInsensitiveString struct {
+	RuneSlice
+}
+
+func (cis CaseInsensitiveString) Less(i, j int) bool {
+	return unicode.ToUpper(cis.RuneSlice[i]) < unicode.ToUpper(cis.RuneSlice[j])
+}
+
+func TestIsPalindromeWithCaseInsentiveString(t *testing.T) {
+	tests := []struct {
+		s    string
+		want bool
+	}{
+		{"", true},
+		{"a", true},
+		{"Aa", true},
+		{"Aba", true},
+		{"Abc", false},
+		{"AbBa", true},
+		{"ABBA", true},
+		{"élle", false},
+		{"éllÉ", true},
+	}
+	for _, test := range tests {
+		got := IsPalindrome(CaseInsensitiveString{RuneSlice(test.s)})
+		if got != test.want {
+			t.Errorf("IsPalindrome(%q): got %t, want %t", test.s, got, test.want)
+		}
+	}
 }
