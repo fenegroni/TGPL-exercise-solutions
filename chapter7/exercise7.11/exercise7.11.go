b2c5405f0888d4be9fe10a76d7e42c22e0849277
Filippo Negroni <filipponegroni@hotmail.com>
2022-01-17 22:54:58 +0000

    ex7.11 Implement /list

--- chapter7/exercise7.11/exercise7.11.go
+++ chapter7/exercise7.11/exercise7.11.go
@@ -1,5 +1,24 @@
 package exercise7_11
 
-func Exercise7_11() {
+import (
+	"fmt"
+	"net/http"
+)
 
+type dollars float32
+type database map[string]dollars
+
+func (d dollars) String() string {
+	return fmt.Sprintf("$%.2f", d)
+}
+
+func (db database) list(response http.ResponseWriter, request *http.Request) {
+	for item, price := range db {
+		fmt.Fprintf(response, "%s: %s\n", item, price)
+	}
+}
+
+func Exercise7_11() {
+	db := database{"shoes": 50.00, "socks": 5.00}
+	http.DefaultServeMux.HandleFunc("/list", db.list)
 }
