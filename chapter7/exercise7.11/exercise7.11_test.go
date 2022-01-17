6fc4c0342ea49bfb0759e9a21828a56e6350e7aa
Filippo Negroni <filipponegroni@hotmail.com>
2022-01-17 07:49:11 +0000

    ex7.11 Fail if /list endpoint not implemented

--- chapter7/exercise7.11/exercise7.11_test.go
+++ chapter7/exercise7.11/exercise7.11_test.go
@@ -19,5 +19,14 @@ func TestWithDefaultServeMux(t *testing.T) {
 	Exercise7_11()
 	server := httptest.NewServer(http.DefaultServeMux)
 	defer server.Close()
-	http.Get(server.URL + "/list") // TODO handle error
+	listEndpoint := server.URL + "/list"
+	response, err := http.Get(listEndpoint)
+	if err != nil {
+		t.Fatalf("Unexpected error calling GET %s: %q", listEndpoint, err)
+	}
+	expected := 200
+	got := response.StatusCode
+	if got != expected {
+		t.Fatalf("Expected response status code %d, got %d", expected, got)
+	}
 }
