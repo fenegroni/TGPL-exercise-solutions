dd4a5d55f29d7c9f7b6607e9b7684e72a924c9ba
Filippo Negroni <filipponegroni@hotmail.com>
2022-01-24 21:16:52 +0000

    ex 7.9 Add create method

--- chapter7/exercise7.11/exercise7.11.go
+++ chapter7/exercise7.11/exercise7.11.go
@@ -14,23 +14,36 @@ func (d dollars) String() string {
 	return fmt.Sprintf("$%.2f", d)
 }
 
-func (db database) list(response http.ResponseWriter, _ *http.Request) {
+func (db database) list(resp http.ResponseWriter, _ *http.Request) {
 	var items []string
 	for i := range db {
 		items = append(items, i)
 	}
 	sort.Strings(items)
 	for _, i := range items {
-		_, _ = fmt.Fprintf(response, "%s: %s\n", i, db[i])
+		_, _ = fmt.Fprintf(resp, "%s: %s\n", i, db[i])
 	}
 }
 
-func (db database) update(response http.ResponseWriter, request *http.Request) {
-	item := request.URL.Query().Get("item")
-	price := request.URL.Query().Get("price")
+func (db database) update(resp http.ResponseWriter, req *http.Request) {
+	item := req.URL.Query().Get("item")
+	price := req.URL.Query().Get("price")
 	_, ok := db[item]
 	if !ok {
-		http.Error(response, fmt.Sprintf("no such item: %q\n", item), http.StatusNotFound)
+		http.Error(resp, fmt.Sprintf("no such item: %q\n", item), http.StatusNotFound)
+		return
+	}
+	if priceVal, err := strconv.ParseFloat(price, 32); err == nil {
+		db[item] = dollars(priceVal)
+	}
+}
+
+func (db database) create(resp http.ResponseWriter, req *http.Request) {
+	item := req.URL.Query().Get("item")
+	price := req.URL.Query().Get("price")
+	_, ok := db[item]
+	if !ok {
+		http.Error(resp, fmt.Sprintf("no such item: %q\n", item), http.StatusNotFound)
 		return
 	}
 	if priceVal, err := strconv.ParseFloat(price, 32); err == nil {
