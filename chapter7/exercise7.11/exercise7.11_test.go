5beb2021564df7397dbc81c52649608f78f92557
Filippo Negroni <filipponegroni@hotmail.com>
2022-02-02 04:08:17 +0000

    ex7.11 implement TestCreateSameItemAfterUpdateIsABadRequest

--- chapter7/exercise7.11/exercise7.11_test.go
+++ chapter7/exercise7.11/exercise7.11_test.go
@@ -16,14 +16,23 @@ func TestEmptyList(t *testing.T) {
 	/*body, err :=*/ _, _ = io.ReadAll(listResponse.Body)
 }
 
-func TestUseDefaultServeMux(t *testing.T) {
-	defer func(mux *http.ServeMux) {
-		http.DefaultServeMux = mux
-	}(http.DefaultServeMux)
+type SavedServeMux http.ServeMux
+
+func (m *SavedServeMux) restore() {
+	http.DefaultServeMux = (*http.ServeMux)(m)
+}
+
+func setupWithDefaultMux(t *testing.T) *httptest.Server {
+	t.Cleanup((*SavedServeMux)(http.DefaultServeMux).restore)
 	http.DefaultServeMux = http.NewServeMux()
 	Exercise711()
-	server := httptest.NewServer(http.DefaultServeMux)
-	defer server.Close()
+	s := httptest.NewServer(http.DefaultServeMux)
+	t.Cleanup(s.Close)
+	return s
+}
+
+func TestUseDefaultServeMux(t *testing.T) {
+	server := setupWithDefaultMux(t)
 	listUrl := server.URL + "/list"
 	resp, err := http.Get(listUrl)
 	if err != nil {
@@ -36,13 +45,7 @@ func TestUseDefaultServeMux(t *testing.T) {
 }
 
 func TestSimpleSequenceOfSuccessfulCalls(t *testing.T) {
-	defer func(mux *http.ServeMux) {
-		http.DefaultServeMux = mux
-	}(http.DefaultServeMux)
-	http.DefaultServeMux = http.NewServeMux()
-	Exercise711()
-	server := httptest.NewServer(http.DefaultServeMux)
-	defer server.Close()
+	server := setupWithDefaultMux(t)
 	steps := []struct {
 		path string
 		body []byte
@@ -53,6 +56,38 @@ func TestSimpleSequenceOfSuccessfulCalls(t *testing.T) {
 		{"/list", []byte("pants: $30.00\nsocks: $6.00\n")},
 		{"/update?item=pants&price=100", []byte("")},
 		{"/list", []byte("pants: $100.00\nsocks: $6.00\n")},
+	}
+	for stepN, s := range steps {
+		resp, err := http.Get(server.URL + s.path)
+		if err != nil {
+			t.Fatalf("Step %d: GET %s: %s", stepN, s.path, err)
+		}
+		// NOTE deferring Close() is ok if the number of steps is small.
+		//goland:noinspection GoDeferInLoop
+		defer resp.Body.Close()
+		if resp.StatusCode != http.StatusOK {
+			t.Fatalf("step %d: GET %s: response code %d", stepN, s.path, resp.StatusCode)
+		}
+		body, err := io.ReadAll(resp.Body)
+		if err != nil {
+			t.Fatalf("step %d: GET %s: ReadAll response body: %s", stepN, s.path, err)
+		}
+		if bytes.Compare(s.body, body) != 0 {
+			t.Fatalf("step %d: GET %s: response body does not match: want %q, got %q", stepN, s.path, s.body, body)
+		}
+	}
+}
+
+func TestCreateSameItemAfterUpdateIsABadRequest(t *testing.T) {
+	server := setupWithDefaultMux(t)
+	steps := []struct {
+		path string
+		code int
+		body []byte
+	}{
+		{"/create?item=shirt&price=10", http.StatusOK, []byte("")},
+		{"/update?item=shirt&price=30", http.StatusOK, []byte("")},
+		{"/create?item=shirt&price=20", http.StatusBadRequest, []byte("")},
 	}
 	for stepN, s := range steps {
 		resp, err := http.Get(server.URL + s.path)
@@ -74,18 +109,3 @@ func TestSimpleSequenceOfSuccessfulCalls(t *testing.T) {
 		}
 	}
 }
-
-// func TestUnsuccessfulCreateAndUpdate(t *testing.T) {
-// 	steps := []struct {
-// 		path     string
-// 		code     int
-// 		listBody []byte
-// 	}{
-// 		{"update?item=socks&price=6", 200, []byte("shoes: $50.00\nsocks: $5.00\n")},
-// 		{"list", []byte("shoes: $50.00\nsocks: $6.00\n")},
-// 		{"create?item=pants&price=30", []byte("")},
-// 		{"list", []byte("pants: $30.00\nshoes: $50.00\nsocks: $6.00\n")},
-// 		{"create?item=shirt", []byte("")},
-// 		{"list", []byte("pants: $30.00\nshoes: $50.00\nsocks: $6.00\n")},
-// 	}
-// }
