c43b8d85e02fb6a0b835917c49fba3c45058bdf7
Filippo Negroni <filipponegroni@hotmail.com>
2022-02-22 08:01:44 +0000

    ex7.13 Improve test format
    Parse is the function that translates an expression string into an Expr

--- chapter7/exercise7.13/exercise7.13_test.go
+++ chapter7/exercise7.13/exercise7.13_test.go
@@ -1,11 +1,14 @@
 package exercise7_13
 
-import "testing"
+import (
+	"TGPL-exercise-solutions/chapter7/expr"
+	"testing"
+)
 
 func TestExpr(t *testing.T) {
-	e1 := NewExpr("sqrt(A / pi)")
-	e2 := NewExpr("pow(x, 3) + pow(y, 3)")
-	e3 := NewExpr("(F - 32) * 5 / 9")
+	e1, _ := expr.Parse("sqrt(A / pi)")
+	e2, _ := expr.Parse("pow(x, 3) + pow(y, 3)")
+	e3, _ := expr.Parse("(F - 32) * 5 / 9")
 	t.Logf("e1: %v", e1)
 	t.Logf("e2: %v", e2)
 	t.Logf("e3: %v", e3)
