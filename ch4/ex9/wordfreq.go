81f513f65038bb9c8c10c39a4c1b72a50b5d01e6
Filippo Negroni <filipponegroni@hotmail.com>
2022-03-31 00:28:23 +0100

    Try resolving package issue

--- ch4/ex9/wordfreq.go
+++ ch4/ex9/wordfreq.go
@@ -7,12 +7,6 @@ import (
 	"strings"
 )
 
-import (
-	"bufio"
-	"io"
-	"strings"
-)
-
 // WordFreq returns the frequency of each word in input.
 func WordFreq(input io.Reader) map[string]int {
 	words := make(map[string]int)
