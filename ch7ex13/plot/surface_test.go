611c01b5fb584171726fad7f81b1c33ef5673bc8
Filippo Negroni <filipponegroni@hotmail.com>
2022-04-11 22:48:51 +0100

    Implement TestSurface

--- ch7ex13/plot/surface_test.go
+++ ch7ex13/plot/surface_test.go
@@ -1,9 +1,31 @@
 package plot
 
-import "testing"
+import (
+	"bytes"
+	"encoding/xml"
+	"testing"
+)
 
 // TestSurface validates the output of Surface is a valid SVG ready
 // to be embedded in an HTTP response with content type SVG.
 func TestSurface(t *testing.T) {
 	// TODO Implement TestSurface
+	buf := new(bytes.Buffer)
+	written, err := Surface(buf, func(x, y float64) float64 {
+		return 0
+	})
+	if err != nil {
+		t.Fatalf("Surface errored: %s", err)
+	}
+	t.Logf("Surface wrote %d bytes.", written)
+	t.Logf("Surface XML: %s", buf.String())
+	tok, err := xml.NewDecoder(buf).Token()
+	if err != nil {
+		t.Fatalf("Surface errored: %s", err)
+	}
+	if start, ok := tok.(xml.StartElement); ok {
+		if start.Name.Local != "svg" || start.Name.Space != "http://www.w3.org/2000/svg" {
+			t.Fatal("Surface output is not SVG")
+		}
+	}
 }
