14bd2e7f29bc815cb69a9d7f2b8414ce00b1acd3
Filippo Negroni <filipponegroni@hotmail.com>
2022-04-13 00:21:00 +0100

    TestPlot

--- ch7ex13/expr/web/plot_test.go
+++ ch7ex13/expr/web/plot_test.go
@@ -1,9 +1,24 @@
 package web
 
-import "testing"
+import (
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"testing"
+)
 
 // TestPlot validates the output of Plot is a valid SVG response.
 // Validating the SVG itself is done in TestSurface
 func TestPlot(t *testing.T) {
-	// TODO implement TestPlot
+	resp := httptest.NewRecorder()
+	req := httptest.NewRequest("", "/?expr="+url.QueryEscape("1+2"), nil)
+	Plot(resp, req)
+	wantCode := http.StatusOK
+	if got := resp.Result().StatusCode; got != wantCode {
+		t.Fatalf("Response code: got %d, want %d", got, wantCode)
+	}
+	wantType := "image/svg+xml"
+	if got := resp.Result().Header.Get("Content-type"); got != wantType {
+		t.Errorf("Response Content-Type: got %q, want %q", got, wantType)
+	}
 }
