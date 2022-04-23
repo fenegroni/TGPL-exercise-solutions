8053609b0153bcb1aac56b5cb9be656448b015e6
Filippo Negroni <filipponegroni@hotmail.com>
2022-04-23 15:28:50 +0100

    ch7ex14 add TestMinWithSyntaxTree

--- ch7ex14/expr/min_test.go
+++ ch7ex14/expr/min_test.go
@@ -1,1 +1,12 @@
 package expr
+
+import "testing"
+
+func TestMinWithSyntaxTree(t *testing.T) {
+	asTree := min{2, 1}
+	wantResult := 1
+	gotResult := asTree.Eval(nil)
+	if gotResult != wantResult {
+		t.Errorf("Wrong result: want %d, got %d", wantResult, gotResult)
+	}
+}
