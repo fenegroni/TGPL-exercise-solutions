3927ebf59efa5f31693ef61924d12d9ea83b167f
Filippo Negroni <filipponegroni@hotmail.com>
2022-05-04 22:38:19 +0100

    ex7.14 Pass more complex tests with syntax trees

--- ch7ex14/expr/min_test.go
+++ ch7ex14/expr/min_test.go
@@ -2,11 +2,41 @@ package expr
 
 import "testing"
 
-func TestMinWithSyntaxTree(t *testing.T) {
+func TestMinSimpleSyntaxTree(t *testing.T) {
 	asTree := min{literal(2), literal(1)}
 	wantResult := literal(1).String()
 	gotResult := literal(asTree.Eval(nil)).String()
 	if gotResult != wantResult {
-		t.Errorf("Wrong result: want %s, got %s", wantResult, gotResult)
+		t.Errorf("Want %s, got %s", wantResult, gotResult)
+	}
+}
+
+func TestMinSyntaxTreeUsingVar(t *testing.T) {
+	env := Env{"A": 2, "B": 1}
+	asTree := min{Var("A"), Var("B")}
+	wantResult := literal(1).String()
+	gotResult := literal(asTree.Eval(env)).String()
+	if gotResult != wantResult {
+		t.Errorf("Want %s, got %s", wantResult, gotResult)
+	}
+}
+
+func TestMinComplexTree(t *testing.T) {
+	env := Env{"A": 2, "B": 3}
+	asTree :=
+		min{
+			binary{'+',
+				min{Var("A"), Var("B")},
+				literal(1),
+			},
+			binary{'+',
+				min{Var("A"), Var("B")},
+				literal(2),
+			},
+		}
+	wantResult := literal(3).String()
+	gotResult := literal(asTree.Eval(env)).String()
+	if gotResult != wantResult {
+		t.Errorf("Want %s, got %s", wantResult, gotResult)
 	}
 }
