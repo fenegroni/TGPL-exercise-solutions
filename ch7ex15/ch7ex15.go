fcfdbffdffc3e50a26921fbeb47c992581302875
Filippo Negroni <filipponegroni@hotmail.com>
2022-05-10 15:16:21 +0100

    ex7.15 Pass TestVarsDetected
    Begin ExampleCh7ex15

---
+++ ch7ex15/ch7ex15.go
@@ -0,0 +1,5 @@
+package ch7ex15
+
+// Ch7ex15 is a placeholder to bring up ExampleCh7ex15
+func Ch7ex15() {
+}
--- ch7ex15/ch7ex15_test.go
+++ ch7ex15/ch7ex15_test.go
@@ -1,29 +1,44 @@
 package ch7ex15
 
 import (
+	"fmt"
 	"github.com/fenegroni/TGPL-exercise-solutions/ch7ex13/expr"
+	"reflect"
 	"testing"
 )
 
 func TestVarsDetected(t *testing.T) {
-	tests := []string{
-		"x / 2",
+	type varMap map[expr.Var]bool
+	tests := []struct {
+		ex   string
+		want varMap
+	}{
+		{"sin(a)", varMap{"a": true}},
+		{"x / Y", varMap{"Y": true, "x": true}},
+		{"X / y", varMap{"X": true, "y": true}},
+		{"1+1", varMap{}},
+		{"A + B * C / D * (A + B)", varMap{"A": true, "B": true, "C": true, "D": true}},
 	}
 	for _, test := range tests {
-		ex, err := expr.Parse(test)
+		ex, err := expr.Parse(test.ex)
 		if err != nil {
-			t.Errorf("Parse(%q): %s", test, err)
+			t.Errorf("Parse(%q): %s", test.ex, err)
 			continue
 		}
-		vars := make(map[expr.Var]bool)
-		err = ex.Check(vars)
+		got := make(varMap)
+		err = ex.Check(got)
 		if err != nil {
-			t.Errorf("Check(%q): %s", test, err)
+			t.Errorf("Check(%q): %s", test.ex, err)
 			continue
+		}
+		if !reflect.DeepEqual(got, test.want) {
+			t.Errorf("variables in %q: want %v, got %v", test.ex, test.want, got)
 		}
 	}
 }
 
 func ExampleCh7ex15() {
-	// TODO validate an Example can read stdin, write stdout
+	var x string
+	fmt.Scanf("%s", &x)
+	fmt.Printf("%s", x)
 }
