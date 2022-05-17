76a03c6090b61ee9b5cc0f33d95dd75e2240e65f
Filippo Negroni <filipponegroni@hotmail.com>
2022-05-18 00:21:42 +0100

    Completed ex7.15

--- ch7ex15/ch7ex15.go
+++ ch7ex15/ch7ex15.go
@@ -1,28 +1,54 @@
 package ch7ex15
 
 import (
+	"bufio"
 	"fmt"
 	"github.com/fenegroni/TGPL-exercise-solutions/ch7ex13/expr"
 	"log"
+	"os"
+	"strconv"
+	"strings"
 )
 
 // Ch7ex15 reads a single expression from the standard input,
 // prompts the user to provide values for any variables,
 // then evaluates the expression in the resulting environment.
 func Ch7ex15() {
-	type varMap map[expr.Var]bool
-	var inEx string
-	fmt.Print("?> ")
-	// TODO handle error
-	fmt.Scanf("%s", &inEx)
-	ex, err := expr.Parse(inEx)
-	if err != nil {
-		log.Fatalf("error parsing the expression: %s", err)
+	expression := userInput("")
+	if expression == "" {
+		log.Fatalf("quit")
 	}
-	vars := make(varMap)
+	log.Printf("Evaluating expression: %q", expression)
+	ex, err := expr.Parse(expression)
+	if err != nil {
+		log.Fatalf("Parse error: %s", err)
+	}
+	vars := make(map[expr.Var]bool)
 	err = ex.Check(vars)
 	if err != nil {
-		log.Fatalf("error checking the expression: %s", err)
+		log.Fatalf("Check error: %s", err)
 	}
-	// TODO ask for values for each variable
+	env := make(expr.Env)
+	for name := range vars {
+		value, _ := strconv.ParseFloat(userInput(name.String()), 64)
+		env[name] = value
+	}
+	fmt.Printf("result> %f\n", ex.Eval(env))
+}
+
+// TODO Use DCI model in userInput
+
+func userInput(prompt string) (input string) {
+	inputScanner := bufio.NewScanner(os.Stdin)
+	for input == "" {
+		fmt.Printf("%s? ", prompt)
+		if !inputScanner.Scan() {
+			if err := inputScanner.Err(); err != nil {
+				log.Fatalf("Input error: %s", err)
+			}
+			break
+		}
+		input = strings.TrimSpace(inputScanner.Text())
+	}
+	return
 }
